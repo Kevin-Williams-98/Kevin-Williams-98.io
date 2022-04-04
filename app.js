5f4244bd21c8d266595e595a8909f8b988334c3b
Kevin Williams <74985629+Kevin-Williams-98@users.noreply.github.com>
2022-04-04 03:12:03 +0000

    moved folders from root to public html as well as server adjustments with node js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -15,24 +17,33 @@ app.listen(PORT, () => {
 */
 // Allows us to read request body contents
 app.use(
-  "/images",
-  express.static(__dirname + "/images")
+  //"/kevin-williams-98io",
+  express.static(path.join(__dirname,"/kevin-williams-98io/public-html"))
 
   //express.urlencoded({
   //extended: true,
   // })
 );
-//const fs = require("fs");
-const path = require("path");
+
 app.get("/", (request, response) => {
   // const fileName = "../index.html";
   //fs.writeFileSync(fileName, request.body.userinput);
-  response.sendFile(__dirname + "/index.html");
+  console.log(request.url)
+  //response.sendFile(__dirname + request.url);
 
-  //response.sendFile(path.join(__dirname, "/index.html"));
+  response.sendFile(path.join(__dirname, "./public-html/index.html"));
 });
 
-app.listen(3000, () => {
+app.get("/user-login.html", (request, response,html) => {
+  // const fileName = "../index.html";
+  //fs.writeFileSync(fileName, request.body.userinput);
+  console.log(request.url)
+  //response.sendFile(__dirname + request.url);
+
+  response.sendFile(path.join(__dirname, "/public-html/user-login"));
+});
+
+app.listen(PORT, () => {
   console.log("App listening on port 3000");
   console.log(__dirname);
 });
